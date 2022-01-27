904e103e179bffa3a317421b6325af416e83d685
Douglas Lundy <douglaslundy@gmail.com>
2022-01-27 08:32:41 -0300

    update page 404

--- pages/404.js
+++ pages/404.js
@@ -1,45 +1,43 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
-import {useState} from 'react';
-import { useRouter } from 'next/router';
 
 function Titulo(props) {
-    const Tag = props.tag || 'h1';
-    return (
-      <>
-        <Tag>{props.children}</Tag>
-        <style jsx>{`
+  const Tag = props.tag || 'h1';
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
               ${Tag} {
                   color: ${appConfig.theme.colors.neutrals['000']};
                   font-size: 24px;
                   font-weight: 600;
               }
               `}</style>
-      </>
-    );
-  }
+    </>
+  );
+}
 
 
 export default function Pagina404() {
-    return (
-        <>
-           <Box
+  return (
+    <>
+      <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: appConfig.theme.colors.neutrals[200],
+          backgroundImage: 'url(https://wallpapercave.com/wp/wp1810633.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
-        
-             <Image
-              styleSheet={{
-                borderRadius: '60%',
-                marginBottom: '1px',
-              }}
-              src={`https://i.pinimg.com/originals/0e/c0/db/0ec0dbf1e9a008acb9955d3246970e15.gif`}
-            />
-        
+        <Image
+          styleSheet={{
+            borderRadius: '60%',
+            marginBottom: '1px',
+          }}
+          src={`https://i.pinimg.com/originals/0e/c0/db/0ec0dbf1e9a008acb9955d3246970e15.gif`}
+        />
+
       </Box>
-        </>
-    )
+    </>
+  )
 }
