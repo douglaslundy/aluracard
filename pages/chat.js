3ce1d13eebefa7d4c964ca48d3d0e245236fe7ef
Douglas Lundy <douglaslundy@gmail.com>
2022-01-28 11:18:58 -0300

    joining machine

--- pages/chat.js
+++ pages/chat.js
@@ -134,7 +134,7 @@ function MessageList(props) {
         <Box
             tag="ul"
             styleSheet={{
-                overflow: 'scroll',
+                overflow: 'hidden',
                 display: 'flex',
                 flexDirection: 'column-reverse',
                 flex: 1,
